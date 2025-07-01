5032acda8802fd1535f71388a1917b697de92a85
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-01 18:15:34 +0000

    deploy: 8b7daa58247f69007775ad1a05f2c729a2ce71d0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -84,71 +84,71 @@ var store = [{
         "teaser": "/assets/images/bull200px.webp"
       },,{
     "title": "Dit is een test post nr 2",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/en/2025-06-20-dit-is-een-test-post-nr02/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/en/2025-06-20-dit-is-een-test-post-nr02/"
   },{
     "title": "Dit is een test post nr 2",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/nl/2025-06-20-dit-is-een-test-post-nr02/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/nl/2025-06-20-dit-is-een-test-post-nr02/"
   },{
     "title": "Eerste post",
-    "excerpt":"eerste post test.       ","url": "https://taliesinds.github.io//_posts/en/2025-06-21-eerste-post/"
+    "excerpt":"eerste post test.       ","url": "https://polyglot.untra.io/_posts/en/2025-06-21-eerste-post/"
   },{
     "title": "Eerste post",
-    "excerpt":"eerste post test.       ","url": "https://taliesinds.github.io//_posts/nl/2025-06-21-eerste-post/"
+    "excerpt":"eerste post test.       ","url": "https://polyglot.untra.io/_posts/nl/2025-06-21-eerste-post/"
   },{
     "title": "Tweede post",
-    "excerpt":"tweede  post test. blablabla over zeep enzo     ","url": "https://taliesinds.github.io//_posts/en/2025-06-21-tweede-post/"
+    "excerpt":"tweede  post test. blablabla over zeep enzo     ","url": "https://polyglot.untra.io/_posts/en/2025-06-21-tweede-post/"
   },{
     "title": "Tweede post",
-    "excerpt":"tweede  post test. blablabla over zeep enzo     ","url": "https://taliesinds.github.io//_posts/nl/2025-06-21-tweede-post/"
+    "excerpt":"tweede  post test. blablabla over zeep enzo     ","url": "https://polyglot.untra.io/_posts/nl/2025-06-21-tweede-post/"
   },{
     "title": "Dit is een test post",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/en/2025-06-22-dit-is-een-test-post/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/en/2025-06-22-dit-is-een-test-post/"
   },{
     "title": "Dit is een test post",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/nl/2025-06-22-dit-is-een-test-post/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/nl/2025-06-22-dit-is-een-test-post/"
   },{
     "title": "Dit is een test post nr 3",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/en/2025-06-23-dit-is-een-test-post-nr03/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/en/2025-06-23-dit-is-een-test-post-nr03/"
   },{
     "title": "Dit is een test post nr 3",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/nl/2025-06-23-dit-is-een-test-post-nr03/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/nl/2025-06-23-dit-is-een-test-post-nr03/"
   },{
     "title": "Dit is een test post nr 4",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/en/2025-06-24-dit-is-een-test-post-nr04/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/en/2025-06-24-dit-is-een-test-post-nr04/"
   },{
     "title": "Dit is een test post nr 4",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/nl/2025-06-24-dit-is-een-test-post-nr04/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/nl/2025-06-24-dit-is-een-test-post-nr04/"
   },{
     "title": "Dit is een test post nr 5",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/en/2025-06-25-dit-is-een-test-post-nr05/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/en/2025-06-25-dit-is-een-test-post-nr05/"
   },{
     "title": "Dit is een test post nr 5",
-    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://taliesinds.github.io//_posts/nl/2025-06-25-dit-is-een-test-post-nr05/"
+    "excerpt":"What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived...","url": "https://polyglot.untra.io/_posts/nl/2025-06-25-dit-is-een-test-post-nr05/"
   },{
     "title": "Over mij",
-    "excerpt":"some stuff about me    ","url": "https://taliesinds.github.io//about/"
+    "excerpt":"some stuff about me    ","url": "https://polyglot.untra.io/about/"
   },{
     "title": "Over mij",
-    "excerpt":"Hier vertel ik wat over mijzelf.    ","url": "https://taliesinds.github.io//about/"
+    "excerpt":"Hier vertel ik wat over mijzelf.    ","url": "https://polyglot.untra.io/about/"
   },{
     "title": "Post Archive",
-    "excerpt":"   Search through all of my posts by tag!         ","url": "https://taliesinds.github.io//archive.html"
+    "excerpt":"   Search through all of my posts by tag!         ","url": "https://polyglot.untra.io/archive.html"
   },{
     "title": "Alle posts met de tag 'post'",
-    "excerpt":"","url": "https://taliesinds.github.io//tag/post/"
+    "excerpt":"","url": "https://polyglot.untra.io/tag/post/"
   },{
     "title": "Alle posts",
-    "excerpt":"Deze pagina toont een volledig overzicht van alle berichten op de site, met de nieuwste bijdragen bovenaan. Hier kun je eenvoudig door recente updates, artikelen en verhalen bladeren in omgekeerd chronologische volgorde. Of je nu op zoek bent naar de laatste inhoud of oudere berichten wilt teruglezen, dit overzicht biedt...","url": "https://taliesinds.github.io//posts/"
+    "excerpt":"Deze pagina toont een volledig overzicht van alle berichten op de site, met de nieuwste bijdragen bovenaan. Hier kun je eenvoudig door recente updates, artikelen en verhalen bladeren in omgekeerd chronologische volgorde. Of je nu op zoek bent naar de laatste inhoud of oudere berichten wilt teruglezen, dit overzicht biedt...","url": "https://polyglot.untra.io/posts/"
   },{
     "title": "andere dingen",
-    "excerpt":"Op deze pagina vind je een overzicht van uiteenlopende projecten waar ik aan werk. Denk aan 3D-printen, leerbewerking, naaien, het maken van kleine messing onderdelen zoals tassenbeslag, experimenten met AI en nog veel meer creatieve bezigheden. Hier deel ik mijn ervaringen, ideeën en resultaten van allerlei algemene projecten en hobby’s....","url": "https://taliesinds.github.io//projects/"
+    "excerpt":"Op deze pagina vind je een overzicht van uiteenlopende projecten waar ik aan werk. Denk aan 3D-printen, leerbewerking, naaien, het maken van kleine messing onderdelen zoals tassenbeslag, experimenten met AI en nog veel meer creatieve bezigheden. Hier deel ik mijn ervaringen, ideeën en resultaten van allerlei algemene projecten en hobby’s....","url": "https://polyglot.untra.io/projects/"
   },{
     "title": "Handgemaakte Zeep",
-    "excerpt":"Op deze pagina deel ik mijn ervaringen met het maken van handgemaakte zeep, het ontwerpen van verpakkingen en het optimaliseren van het productieproces. Ik ben geen expert, maar vind het leuk om te experimenteren met verschillende recepten, technieken en materialen. Hier lees je over mijn ontdekkingen, successen en soms ook...","url": "https://taliesinds.github.io//soap/"
+    "excerpt":"Op deze pagina deel ik mijn ervaringen met het maken van handgemaakte zeep, het ontwerpen van verpakkingen en het optimaliseren van het productieproces. Ik ben geen expert, maar vind het leuk om te experimenteren met verschillende recepten, technieken en materialen. Hier lees je over mijn ontdekkingen, successen en soms ook...","url": "https://polyglot.untra.io/soap/"
   },{
     "title": "Alle posts met de tag 'webdev'",
-    "excerpt":"","url": "https://taliesinds.github.io//tag/webdev/"
+    "excerpt":"","url": "https://polyglot.untra.io/tag/webdev/"
   },{
     "title": "Alle posts met de tag 'zeep'",
-    "excerpt":"","url": "https://taliesinds.github.io//tag/zeep/"
+    "excerpt":"","url": "https://polyglot.untra.io/tag/zeep/"
   }]

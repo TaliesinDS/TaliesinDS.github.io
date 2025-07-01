807e26ec4fdb2f4dae8c7cda728f99722de2ba8b
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-01 22:35:50 +0000

    deploy: 5c4140b0bdd04374afa58de02fc5dfa9cc7a65cc

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,7 @@ var store = [,{
   },{
     "title": "andere dingen",
     "excerpt":"Op deze pagina vind je een overzicht van uiteenlopende projecten waar ik aan werk. Denk aan 3D-printen, leerbewerking, naaien, het maken van kleine messing onderdelen zoals tassenbeslag, experimenten met AI en nog veel meer creatieve bezigheden. Hier deel ik mijn ervaringen, ideeën en resultaten van allerlei algemene projecten en hobby’s....","url": "https://taliesinds.github.io/en/"
+  },{
+    "title": "Tags",
+    "excerpt":"   Browse posts by tag:      {% for tag in site.data.tags %}                     #{{ tag[0] }}               - {{ tag[1].description }}        {% endfor %} ","url": "https://taliesinds.github.io/:lang/tags/"
   }]

876441c9facbcc8ab4cfc93c6843c68881046943
TaliesinDS <35978059+TaliesinDS@users.noreply.github.com>
2025-07-04 16:35:59 +0200

    Update lang-toggle.js

--- assets/js/lang-toggle.js
+++ assets/js/lang-toggle.js
@@ -6,14 +6,14 @@ function toggleLang() {
   if (currentLang === 'en') {
     enBlocks.forEach(el => el.style.display = 'none');
     nlBlocks.forEach(el => el.style.display = 'block');
-    flag.src = "assets/images/ui/gb.svg"; // English flag (US)
-    flag.alt = "English flag";
+    flag.src = "/assets/images/ui/gb.svg"; // English flag (US)
+    flag.alt = "English";
     currentLang = 'nl';
   } else {
     enBlocks.forEach(el => el.style.display = 'block');
     nlBlocks.forEach(el => el.style.display = 'none');
-    flag.src = "assets/images/ui/nl.svg"; // Dutch flag (NL)
-    flag.alt = "Dutch flag";
+    flag.src = "/assets/images/ui/nl.svg"; // Dutch flag (NL)
+    flag.alt = "Dutch";
     currentLang = 'en';
   }
 }

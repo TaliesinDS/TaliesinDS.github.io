4ea7014bea616c2119e48e64d0b718ed59302f37
TaliesinDS <35978059+TaliesinDS@users.noreply.github.com>
2025-07-04 16:32:33 +0200

    flags

--- assets/js/lang-toggle.js
+++ assets/js/lang-toggle.js
@@ -6,13 +6,13 @@ function toggleLang() {
   if (currentLang === 'en') {
     enBlocks.forEach(el => el.style.display = 'none');
     nlBlocks.forEach(el => el.style.display = 'block');
-    flag.src = "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f1fa-1f1f8.svg"; // English flag (US)
+    flag.src = "assets/images/ui/gb.svg"; // English flag (US)
     flag.alt = "English flag";
     currentLang = 'nl';
   } else {
     enBlocks.forEach(el => el.style.display = 'block');
     nlBlocks.forEach(el => el.style.display = 'none');
-    flag.src = "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f1ec-1f1e7.svg"; // Dutch flag (NL)
+    flag.src = "assets/images/ui/nl.svg"; // Dutch flag (NL)
     flag.alt = "Dutch flag";
     currentLang = 'en';
   }

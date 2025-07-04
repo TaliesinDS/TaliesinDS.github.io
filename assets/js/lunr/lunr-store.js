59f5593b80ac9693def125a1bc6d01a5b7fbebe9
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-04 14:24:00 +0000

    deploy: 9e6d69ba9ae1436c39e23b21d9a388f471017f6f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -42,14 +42,14 @@ var store = [{
         "teaser": "/assets/images/bull200px.webp"
       },{
         "title": "2 Talige Versie Test Post",
-        "excerpt":"Dit is de samenvatting of inleiding van je post.        Nederlands      About Me    Hello! My name is Arthur...       Over mij    Hallo! Mijn naam is Arthur...    ","categories": [],
+        "excerpt":"Dit is de samenvatting of inleiding van je post.                      About Me    Hello! My name is Arthur...       Over mij    Hallo! Mijn naam is Arthur...   ","categories": [],
         "tags": ["post","taal","test"],
         "url": "/2-talige-versie-test-post/",
         "teaser": "/assets/images/bull200px.webp"
       },{
         "title": "post generator test",
         "excerpt":"Dit is de samenvatting of inleiding van je post.     ","categories": [],
-        "tags": [],
+        "tags": ["test"],
         "url": "/post-generator-test/",
         "teaser": "/assets/images/bull200px.webp"
       },,]

f0481289490dd4dc9e5592db32b51e40cca1e766
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-07 05:19:50 +0000

    deploy: d0a145a7e1d34fae58bb254794f90fb7490ba033

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -55,7 +55,7 @@ var store = [{
       },{
         "title": "Over Geurstoffen en de Veiligheid van Mijn Zeep",
         "excerpt":"Over Geurstoffen en de Veiligheid van Mijn Zeep Al mijn handgemaakte zepen worden met zorg samengesteld, met jouw veiligheid als hoogste prioriteit. Voor de geur gebruik ik hoogwaardige geurstoffen, waarbij elke ingrediënt zorgvuldig is gekozen op basis van geur én veiligheid. Over mij Over Geurstoffen en de Veiligheid van Mijn...","categories": [],
-        "tags": [],
+        "tags": ["zeep"],
         "url": "/over-geurstoffen-en-de-veiligheid-van-mijn-zeep/",
         "teaser": "/assets/images/bull200px.webp"
       },,]

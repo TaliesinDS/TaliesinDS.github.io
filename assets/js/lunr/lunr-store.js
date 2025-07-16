e592f7b8dc4bb52e7219bb9868cef6d0c9ad1e7d
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-16 16:13:16 +0000

    deploy: 56d24c449f7b9590f956b4c4847c273279416147

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -54,7 +54,7 @@ var store = [{
         "teaser": "/assets/images/bull200px.webp"
       },{
         "title": "Aqua di Gio Zeep",
-        "excerpt":"Aqua di Gio Zeep Traditionele talg, kokos en olijfoliezeep met luxe geur Hydraterend, duurzaam en huidvriendelijk Aqua di Gio Zeep Traditionele talg, kokos en olijfoliezeep met luxe geur Hydraterend, duurzaam en huidvriendelijk Lange zomerdagen aan zee, de zonnestralen op de huid en de verfrissende bries die van de oceaan komt....","categories": [],
+        "excerpt":"Traditionele talg, kokos en olijfoliezeep met luxe geur Hydraterend, duurzaam en huidvriendelijk Aqua di Gio Zeep Traditionele talg, kokos en olijfoliezeep met luxe geur Hydraterend, duurzaam en huidvriendelijk Lange zomerdagen aan zee, de zonnestralen op de huid en de verfrissende bries die van de oceaan komt. Een contrast van sprankelende...","categories": [],
         "tags": ["zeep"],
         "url": "/aqua-di-gio-zeep/",
         "teaser": "/assets/images/bull200px.webp"

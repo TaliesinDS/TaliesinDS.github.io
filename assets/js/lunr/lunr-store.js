f858acbb105311a94dc2936f931fb74882dce573
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-16 12:53:00 +0000

    deploy: 94c55c04c516d6c7b7afc49d21c896f41d2175b4

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -60,7 +60,7 @@ var store = [{
         "teaser": "/assets/images/bull200px.webp"
       },{
         "title": "Naturel Zeep",
-        "excerpt":"Handgemaakte Zeep Traditionele Talg-, Kokos- &amp; Olijfoliezeep – Hydraterend, Duurzaam en Huidvriendelijk Ervaar het verschil van een zorgvuldig gemaakte, ambachtelijke zeep op basis van een klassieke mix van rundertalg, kokosolie en olijfolie. Deze formule is ontworpen voor een uitgebalanceerde zeep die effectief reinigt en toch mild en hydraterend blijft. Hoogwaardige...","categories": [],
+        "excerpt":"Handgemaakte Zeep Handgemaakte Zeep Traditionele Talg-, Kokos- &amp; Olijfoliezeep – Hydraterend, Duurzaam en Huidvriendelijk Ervaar het verschil van een zorgvuldig gemaakte, ambachtelijke zeep op basis van een klassieke mix van rundertalg, kokosolie en olijfolie. Deze formule is ontworpen voor een uitgebalanceerde zeep die effectief reinigt en toch mild en hydraterend...","categories": [],
         "tags": ["zeep"],
         "url": "/naturel-zeep/",
         "teaser": "/assets/images/bull200px.webp"

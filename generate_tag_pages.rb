23e42443f7bd278c0e7d1978616d30d13f0028b2
TaliesinDS <TaliesinDS@users.noreply.github.com>
2025-07-17 15:56:14 +0000

    deploy: 9ca5f311d489d8603fd7a4b245636984f28e8c4d

--- generate_tag_pages.rb
+++ generate_tag_pages.rb
@@ -79,10 +79,19 @@ begin
         {% assign tagname = page.name | split: '.' | first %}
         {% assign tagged_posts = site.posts | where_exp: "post", "post.tags contains tagname" %}
             {% for post in tagged_posts %}
+                {% if post.header and post.header.teaser %}
+                    {% assign teaser = post.header.teaser %}
+                {% elsif post.header and post.header['teaser'] %}
+                    {% assign teaser = post.header['teaser'] %}
+                {% elsif post['header'] and post['header']['teaser'] %}
+                    {% assign teaser = post['header']['teaser'] %}
+                {% else %}
+                    {% assign teaser = '/assets/images/bull200px.webp' %}
+                {% endif %}
                 <a href="{{ post.url | relative_url }}" class="tag-post-item-link">
                     <div class="tag-post-item">
                         <div class="tag-post-teaser">
-                            <img src="{{ post.teaser | default: '/assets/images/bull200px.webp' }}" alt="Teaser" class="tag-post-img">
+                            <img src="{{ teaser | relative_url }}" alt="Teaser" class="tag-post-img">
                         </div>
                         <div class="tag-post-content">
                             <h4 class="tag-post-title">
